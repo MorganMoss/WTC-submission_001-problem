fcc8cb3c505f623833b3bf0f84e8a76103d25dcf
mmoss <mmoss020@student.wethinkcode.co.za>
2021-10-04 10:55:39 +0200

    Completed

--- course.py
+++ course.py
@@ -1,10 +1,39 @@
+def create_outline():    
+
+    course_topics = {
+        "Introduction to Python", "Tools of the Trade", 
+        "How to make decisions", "How to repeat code", 
+        "How to structure data", "Functions", "Modules"}
+
+    problem_map = dict()
+
+    student_progress = [
+        ("Morgan", "Functions", "Problem 3", "[GRADED]"),
+        ("Lee", "How to structure data", "Problem 1","[COMPLETED]"),
+        ("Willa", "How to make decisions", "Problem 2", "[STARTED]")]
+
+    course_topics = list(course_topics)
+    course_topics.sort()
+
+    def get_key(item):
+        return item[3]
+    student_progress.sort(key = get_key, reverse = True)
 
 
-def create_outline():
-    """
-    TODO: implement your code here
-    """
-    pass
+    print("Course Topics:")
+    for topic in course_topics:
+        print("* {0}".format(topic)) 
+        problem_map[topic] = [
+            "Problem 1", "Problem 2", "Problem 3"]
+
+    print("Problems:")
+    for topic, problems in problem_map.items():
+        print("* {0} : ".format(topic), end="")
+        print(*problems, sep = ", ")
+    
+    print("Student Progress:")
+    for i, entry in enumerate(student_progress):
+        print("{0}. {1[0]} - {1[1]} - {1[2]} - {1[3]}".format(i+1, entry))
 
 
 if __name__ == "__main__":
